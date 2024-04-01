915e18133e8692fc6a4a2416c31716279ac448db
yeonuKim <ywk0524@snu.ac.kr>
2024-04-01 19:35:13 +0900

    [feat] Add css of a note component

--- memo_app/src/components/Note.js
+++ memo_app/src/components/Note.js
@@ -1,12 +1,13 @@
 import { MdDeleteForever } from 'react-icons/md';
+import styles from "./Note.module.css";
 
 const Note = () => {
     return (
-        <div className="note">
+        <div className={styles.note}>
             <span>Hi</span>
-            <div className="note-footer">
+            <div className={styles.note_footer}>
                 <small>010424</small>
-                <MdDeleteForever className="delete-icon" size="1.3em"/>
+                <MdDeleteForever className={styles.delete_icon} size="1.3em"/>
             </div>
         </div>
     )
