726c65eb6cbc9ec4ffb098d3913d97c3c296f107
yeonuKim <ywk0524@snu.ac.kr>
2024-04-05 23:09:09 +0900

    [feat] Set mongoDB

--- express_post/app.js
+++ express_post/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
+const { secret } = require("./configs/secret");
+const mongoose = require("mongoose");
 
 // Set view engine to handlebars
 app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
@@ -22,6 +24,17 @@ app.get("/detail/:id", async (req, res) => {
   });
 });
 
+// Check the express connection
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// Check the mongoDB connection
+mongoose
+  .connect(secret.mongoURI)
+  .then(() => {
+    console.log("MongoDB connected...");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
