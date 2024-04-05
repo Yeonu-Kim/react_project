c5c3768f4c4757014c039a6ae1d6d37aaaec77d6
yeonuKim <ywk0524@snu.ac.kr>
2024-04-05 22:00:57 +0900

    [feat] Make post creation page UI

--- express_post/app.js
+++ express_post/app.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.render("home", { title: "테스트 게시판" });
 });
 
+app.get("/write", (req, res) => {
+  res.render("write", { title: "테스트 게시판" });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
