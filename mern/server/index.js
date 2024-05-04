3d9649bb7ca95854f0cec6c8ca95bb2daf9e8231
yeonuKim <ywk0524@snu.ac.kr>
2024-05-04 14:11:33 +0900

    [feat] Connect mongodb

--- mern/server/index.js
+++ mern/server/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const dotoenv = require("dotenv")
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 7000;
 
+// Env Setting
+dotoenv.config();
+
+app.get("/", (req, res) => {
+    res.json("Hello World!");
+});
+
+// Check the mongoDB connection
+const connectDB = async () => {
+    console.log(process.env.MONGO);
+    
+    mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("MongoDB connected...");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
 // Check server listen
 app.listen(port, () => {
+    connectDB();
     console.log(`Listening on port ${port}`);
   });
